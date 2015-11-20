dd63da4041175935c2df56fa7367d848166abd65
Watthanachai Dueanklang <zaintcorp@yahoo.com>
2015-11-20 13:25:03 +0700

    modified:   Makefile 	new file:   function.c 	new file:   function.h 	modified:   main.c 	new file:   opt.c 	new file:   opt.h

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 CC=gcc
 bin=dotbox
-CFLAGS=-O2 -std=c89 -pedantic -Wall -Werror
+CFLAGS=-O2 -std=c89 -pedantic #-Wall -Werror
 .PHONY: all clean
 
 all: $(bin)
 
-$(bin):	main.o  dotbox.o dotbox_io.o
-	$(CC) -o $(bin) main.o  dotbox.o dotbox_io.o
+$(bin):	main.o  dotbox.o dotbox_io.o function.o opt.o
+	$(CC) -o $(bin) main.o  dotbox.o dotbox_io.o function.o opt.o
 
 clean:
 	rm -rf *.o *.exe $(bin)
@@ -18,5 +18,8 @@ dotbox.o: dotbox.c dotbox.h
 dotbox_io.o: dotbox_io.c dotbox.h dotbox_io.h
 
 main.o: main.c dotbox.h
-	$(CC) $(CFLAGS) -c main.c
+
+function.o: function.c function.h
+
+opt.o: opt.c opt.h
 
--- main.c
+++ main.c
@@ -1,37 +1,88 @@
 #include <stdio.h>
 #include "dotbox.h"
 #include "dotbox_io.h"
+#include "function.h"
 
 #define D_SQR 3
 #define LEN 4
+#define BSIZE 512
+
+#define PREFIX_X 'x'
+#define PREFIX_Y 'y'
+
+static int cropui(struct dbs_game *game,const char *str,struct dbs_line *line);
 
 int main(void)
 {
 	int n,j;
+	unsigned int pindex;
 	struct dbs_game game;
 	struct dbs_line line;
 	
+	static char buff[BSIZE+1];
+	
+	pindex=0;
 	dbf_init(&game,"Hwoy",D_SQR);
 
 
-printTable(&game,LEN);
-
-
+	putchar('\n');
+	printTable(&game,LEN);
+	putchar('\n');
+	
 do
 {
 	
-	n=game.ai(&game,&line);
-	j=dbf_gameplay(&game,&line,&game.player[1]);
+	if(pindex==1)
+	{
+		n=game.ai(&game,&line);
+	}
+	else if(pindex==0)
+	{
+		do
+		{
+		dio_getch(buff,BSIZE,0);
+		}while(cropui(&game,buff,&line)<0);
+	}
+	j=dbf_gameplay(&game,&line,&game.player[pindex]);
 	
-	printf("return AI = %d, (%u,%u)  (%u,%u)\n",n,line.p1.x,line.p1.y,line.p2.x,line.p2.y);
+	if(pindex==1) printf("return AI = %d, (%u,%u)  (%u,%u)\n",n,line.p1.x,line.p1.y,line.p2.x,line.p2.y);
+	printf("%s:Score= %u\n",game.player[0].name,game.player[0].score);
 	printf("%s:Score= %u\n",game.player[1].name,game.player[1].score);
 	
+	putchar('\n');
 	printTable(&game,LEN);
+	putchar('\n');
+	
+	if(pindex==1) pindex=0;
+	else pindex=1;
 
 }while(j!=gp_gameover);
 
 
 	dbf_destroy(&game);
 	return 0;
+}
+
+static int cropui(struct dbs_game *game,const char *str,struct dbs_line *line)
+{
+	unsigned int x,y;
+	if(str[0]==PREFIX_X)
+	{
+		if(!isUint(&str[1])) return -1;
+		x=s2ui(&str[1]);
+		dbf_getpointlinex(game,x,line);
+	}
+	else if(str[0]==PREFIX_Y)
+	{
+		if(!isUint(&str[1])) return -1;
+		y=s2ui(&str[1]);
+		dbf_getpointliney(game,y,line);
+	}
+	else
+	{
+		return -2;
+	}
+	
+	return 1;
 }
 
